fae644d0fc8df7726d1dafbd38a39db4bf2442fc
BershadskiyOleg <bershadskiy.oleg@ukr.net>
2021-07-09 12:25:02 +0300

    code for Lesson 1 task 5

---
+++ Lesson1_Task_5/Lesson 1 TASK 1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number_1, number_2, action;
+    cout << "Vvedite perve chislo: ";
+    cin >> number_1;
+    cout << "Vvedite dryge chislo: ";
+    cin >> number_2;
+    cout << "Vuberite diay: \n" << "1 - dodatu\n" << "2 - vidnaytu\n" << "3 - pomnogutu\n" << "4 - podilutu\n";
+    cin >> action;
+
+    switch(action){
+    case 1 : cout << "Resultat: " << number_1 + number_2; break;
+    case 2 : cout << "Resultat: " << number_1 - number_2; break;
+    case 3 : cout << "Resultat: " << number_1 * number_2; break;
+    case 4 : cout << "Resultat: " << number_1 / number_2; break;
+    default: cout << "Taka diay vidsytnay";
+    }
+
+}
