021ff0d0cdd05b8ec70ffaf3d809696c082d1e0f
BershadskiyOleg <bershadskiy.oleg@ukr.net>
2021-07-09 12:21:21 +0300

    code for Lesson 1 Task 4

---
+++ Lesson1_Task_4/Lesson 1 TASK 4/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   float rozmir_zarplatu;
+   cout << "Vvedite rozmir zarplatu: ";
+   cin >> rozmir_zarplatu;
+
+
+   if(rozmir_zarplatu > 1000000)
+   {
+       cout << "Da ti millioner";
+   }
+
+   if(rozmir_zarplatu > 1000 & rozmir_zarplatu < 1000000)
+   {
+       cout << "Kruto, no ti molodec";
+   }
+   if(rozmir_zarplatu < 1000)
+   {
+       cout << "Rabotai bolshe, no ti molodec";
+   }
+
+       return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
